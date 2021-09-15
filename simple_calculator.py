afef9b41fa5edb2e9f9f965f4e064331822c058c
Sergio Santoyo <santoyosergio14@gmail.com>
2021-09-15 13:59:56 -0700

    Added input checker plus actions

--- simple_calculator.py
+++ simple_calculator.py
@@ -20,3 +20,17 @@ print("4. Divide")
 while True:
     #Takes the input from the user
     choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ("1", "2", "3", "4"):
+        print("Start by entering values!")
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if(choice == "1"):
+            print(num1, "+", num2, "=", add(num1, num2))
+        if(choice == "2"):
+             print(num1, "-", num2, "=", subtract(num1, num2))
+        if(choice == "3"):
+             print(num1, "*", num2, "=", multiply(num1, num2))
+        if(choice == "4"):
+             print(num1, "/", num2, "=", divide(num1, num2))
