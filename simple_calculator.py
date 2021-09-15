053f1551931e939e0b8eb872b13a172dbea43eaf
Sergio Santoyo <santoyosergio14@gmail.com>
2021-09-15 12:40:36 -0700

    Added calculation functions

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,1 +1,22 @@
-print("hello world")
+#add function
+def add(x,y):
+    return x+y
+#subtract function
+def subtract(x,y):
+    return x-y
+#multiply function
+def multiply(x,y):
+    return x*y
+#divide function
+def divide(x,y):
+    return x/y
+
+print("Select operation of calculation")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    #Takes the input from the user
+    choice = input("Enter choice(1/2/3/4): ")
