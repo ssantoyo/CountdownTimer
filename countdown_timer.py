2f479136fca20ef0c980df6a1aa66e7101426486
Sergio Santoyo <santoyosergio14@gmail.com>
2021-09-15 17:11:38 -0700

    Finalized timer, converts sec to min

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,1 +1,18 @@
-print("Hello Sergio")
+#time module
+import time
+
+#countdown function
+def countdown(t):
+    while t:
+        min, sec = divmod(t,60)
+        timer = '{:02d}:{:02d}'.format(min,sec)
+        print(timer, end="\r")
+        time.sleep(1)
+        t-=1
+
+    print("Timer is UP!!!")
+
+#input time in seconds
+t = input("Enter the time in seconds: ")
+
+countdown(int(t))
